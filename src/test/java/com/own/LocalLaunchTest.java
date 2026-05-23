1ab28702e4caae1108fe2dbb87a28a41f41720c9
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-05-23 07:35:36 +0200

    Seperate headless and head mode into two method.

--- src/test/java/com/own/LocalLaunchTest.java
+++ src/test/java/com/own/LocalLaunchTest.java
@@ -12,20 +12,28 @@ public class LocalLaunchTest extends BaseFormTest {
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
+        headlessMode(options);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    ChromeOptions headlessMode(ChromeOptions options) {
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--headless=new");
 
         // Itt inicializáljuk a LOKÁLIS drivert.
         // Ezzel nem nyílik meg a böngésző.
         driver = new ChromeDriver(options);
+        return options;
+    }
 
+    /** Ha ezt állítod be, akkor a headlessMode-ot ne hívd meg. */
+    void headMode() {
         // Ezzel megnyílik a böngésző, viszont a GitHub Actions el fog hasalni!
-        // driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         // A GitHub Actions elhasal ezzel, mert nincsen böngésző, amit maximalizálni
         // lehetne.
-        // driver.manage().window().maximize();
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
     }
 }
